a270cb227c8ea033a9e2cc8eea5014fefa120051
Mochammad Hifzhisyaukani <mochammadhifzhisyaukani@smkwikrama.sch.id>
2026-03-27 10:33:31 +0700

    halaman login dan logout

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let daftar = [];
-let transaksi =[];
+let transaksi = [];
 let editIndex = null;
 
+// nambah buku
 function tambah() {
   let judul = document.getElementById("judul").value;
   let penulis = document.getElementById("penulis").value;
-  let genre = document.getElementById("genre").value;
+  // let genre = document.getElementById("genre").value;
   let harga = Number(document.getElementById("harga").value);
   let stok = document.getElementById("stok").value;
 
@@ -15,10 +16,8 @@ function tambah() {
   } else if (!penulis) {
     alert("tolong masukan penulis terlebih dahulu");
     return;
-  } else if (!genre)  {
-    alert("tolong masukkan genre terlebih dahulu");
-    return;
-  } else if (!harga) {
+  } 
+  else if (!harga) {
     alert("tolong masukan harga terlebih dahulu");
     return;
   } else if (!stok) {
@@ -29,13 +28,12 @@ function tambah() {
   let data = {
     judul: judul,
     penulis: penulis,
-    genre: genre,
     harga: harga,
     stok: stok,
   };
 
   if (editIndex !== null) {
-    daftar[editIndex] = { judul, penulis, genre, harga, stok };
+    daftar[editIndex] = { judul, penulis, harga, stok };
     editIndex = null;
     document.getElementById("Tambah").innerText = "Tambah";
   } else {
@@ -44,21 +42,67 @@ function tambah() {
 
   clearForm();
   render();
-
 }
 
+// hapus semua
 function ClearAll() {
-  daftar = [];
-  render();
-
+  let hapus = confirm("apakah anda akan menghapus semuanya?");
+  if (hapus) {
+    daftar = [];
+    transaksi = [];
+    render();   
+  }
 }
 
+//hapus yang ada di form biar ga berantakan history nya
+function clearForm() {
+  document.getElementById("judul").value = "";
+  document.getElementById("penulis").value = "";
+  // document.getElementById("genre").value = "";
+  document.getElementById("harga").value = "";
+  document.getElementById("stok").value = "";
+}
+
+//untuk munculin buku / update lagi buku semua
+function render() {
+  const listBuku = document.getElementById("listBuku");
+  const listBeli = document.getElementById("listBeli");
+
+  listBuku.innerHTML = daftar
+    .map(
+      (b, i) => `
+        <div class="card-js">
+          <h3>${b.judul}</h3>
+          <p>Penulis: ${b.penulis}</p>
+          <p>Harga: ${b.harga.toLocaleString()}</p>
+          <p>Stok: ${b.stok}</p>
+          <button onclick="editBook(${i})" class="btn">Edit</button>
+          <button onclick="deleteBook(${i})" class="btn">Hapus</button>
+        </div>`,
+    )
+    .join("");
+
+  listBeli.innerHTML = daftar
+    .map(
+      (b, i) => `
+        <div class="card-js">
+          <h3>${b.judul}</h3>
+          <p>Penulis: ${b.penulis}</p>
+          <p>Harga: ${b.harga.toLocaleString()}</p>
+          <p>Stok: ${b.stok}</p>
+          <button onclick="beliBook(${i})" class="btn">Beli</button>
+        </div>`,
+    )
+    .join("");
+  tampilkanStruk();
+}
+
+// untuk edit buku
 function editBook(index) {
   const b = daftar[index];
 
   document.getElementById("judul").value = b.judul;
   document.getElementById("penulis").value = b.penulis;
-  document.getElementById("genre").value = b.genre;
   document.getElementById("harga").value = b.harga;
   document.getElementById("stok").value = b.stok;
 
@@ -66,40 +110,8 @@ function editBook(index) {
   document.getElementById("Tambah").innerText = "Update";
 }
 
-function render() {
-  const listBuku = document.getElementById("listBuku");
-  const listBeli = document.getElementById("listBeli");
 
-  listBuku.innerHTML = daftar
-    .map((b, i) => `
-        <div class="card-js">
-          <h3>${b.judul}</h3>
-          <p>Penulis: ${b.penulis}</p>
-          <p>Genre: ${b.genre}</p>
-          <p>Harga: ${b.harga}</p>
-          <p>Stok: ${b.stok}</p>
-          <button onclick="editBook(${i})" class="btn">Edit</button>
-          <button onclick="deleteBook(${i})" class="btn">Hapus</button>
-        </div>`
-    ).join("");
-      
-  listBeli.innerHTML = daftar.map((b, i) => `
-        <div class="card-js">
-          <h3>${b.judul}</h3>
-          <p>Penulis: ${b.penulis}</p>
-          <p>Genre: ${b.genre}</p>
-          <p>Harga: ${b.harga}</p>
-          <p>Stok: ${b.stok}</p>
-          <button onclick="beliBook(${i})" class="btn">Beli</button>
-        </div>`
-      ).join("");;
-      tampilkanStruk();
-
-
-    }
-    
-    
-
+// untuk beli buku
 function beliBook(index) {
   let buku = daftar[index];
 
@@ -109,13 +121,14 @@ function beliBook(index) {
     let dataTransaksi = {
       id: "TRX-" + Date.now(),
       judul: buku.judul,
-      genre: buku.genre,
+      // genre: buku.genre,
       harga: buku.harga,
       tanggal: new Date().toLocaleString(),
     };
 
     transaksi.push(dataTransaksi);
     alert("Buku berhasil dibeli!");
+
   } else {
     alert("Stok habis!");
   }
@@ -123,11 +136,13 @@ function beliBook(index) {
   tampilkanStruk();
 }
 
+//untuk delete buku 1 per 1
 function deleteBook(index) {
   daftar.splice(index, 1);
   render();
 }
 
+//untuk tampilin s truk 
 function tampilkanStruk() {
   let container = document.getElementById("strukTransaksi");
   if (!container) return;
@@ -139,7 +154,6 @@ function tampilkanStruk() {
       <div class="struk">
         <b>ID: ${t.id}</b><br>
         Judul: ${t.judul}<br>
-        Genre: ${t.genre}<br>
         Harga:  Rp ${t.harga}<br>
         Tanggal: ${t.tanggal}
       </div>
@@ -147,16 +161,14 @@ function tampilkanStruk() {
   });
 }
 
-
-
-function clearForm() {
-  document.getElementById("judul").value = "";
-  document.getElementById("penulis").value = "";
-  document.getElementById("genre").value = "";
-  document.getElementById("harga").value = "";
-  document.getElementById("stok").value = "";
+// logout
+function logout() {
+  let confirmLogout = confirm("Apakah Anda yakin ingin logout?");
+  if (confirmLogout) {
+    window.location.href = "login.html";
+  } else {
+    alert("Logout dibatalkan.");
+  }
 }
-
-
-
+// update 
 render();
